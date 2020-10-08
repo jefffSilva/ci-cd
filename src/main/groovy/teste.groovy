76dd0a030c4c06244064f23b3a0485f844ea1f43
jeferson <jefersonlinuxextreme@gmail.com>
2020-10-08 18:33:23 +0000

    agora vai v8

--- src/main/groovy/teste.groovy
+++ src/main/groovy/teste.groovy
@@ -3,7 +3,7 @@ class Example {
       Integer x = 3,y = 10,z = 0; 
       z = x+y; 
       println(z);
-      println ("Hello Santander, I am here to be crazy like... Aloizio is" (z));
    }
 }
 
+println "Hello Santander, I am here to be crazy like... Aloizio is" 
