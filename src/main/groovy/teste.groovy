2950d0e648b1ecd95dd62808bb354d06b29e15a4
jeferson <jefersonlinuxextreme@gmail.com>
2020-10-08 18:31:02 +0000

    agora vai v7

--- src/main/groovy/teste.groovy
+++ src/main/groovy/teste.groovy
@@ -1,1 +1,9 @@
-println "Hello Santander, I am here to be crazy like Aloizio"
+class Example { 
+   static void main(String[] args) {
+      Integer x = 3,y = 10,z = 0; 
+      z = x+y; 
+      println(z);
+      println ("Hello Santander, I am here to be crazy like... Aloizio is" (z));
+   }
+}
+
